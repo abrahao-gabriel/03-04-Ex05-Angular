a7d5c6e67084fbd76263106be8ee65884023b5e3
abrahao-gabriel <gabriel17.abrahao@gmail.com>
2022-04-03 18:46:25 -0300

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -6,9 +6,17 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { RouterModule } from '@angular/router';
+import { ListDetailsComponent } from './list-details/list-details.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot([
+    {path: '' , component: HomeComponent},
+    {path: 'list' , component: ListComponent},
+    {path: 'list/:index' , component: ListDetailsComponent}
+
+  ]
+  )],
   declarations: [AppComponent, HelloComponent, HomeComponent, ListComponent],
   bootstrap: [AppComponent],
 })
