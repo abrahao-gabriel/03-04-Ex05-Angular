bb88b7e2320cb3fa3cfe32c4d59be147c36a7bc7
abrahao-gabriel <gabriel17.abrahao@gmail.com>
2022-04-03 19:49:14 -0300

    4 files modified, 2 added

---
+++ src/app/list.ts
@@ -0,0 +1,41 @@
+export const listData = [
+  {
+    cod: 'IDS-002',
+    nome: 'Desenvolvimento para Servidores II',
+    dia: 'Quinta-feira',
+    horario: '19:00',
+    objetivo:
+      'Desenvolver um site completo de e-commerce ou outro tipo de negócio na Internet usando uma linguagem apropriada a servidores, banco de dados e padrões de projeto.',
+    ementa:
+      'Conceitos e evolução das tecnologias de programação de servidores. Recursos da linguagem escolhida para servidores na Internet. Padrões de projetos. Integração com sistemas (Google Maps API, Twitter, entre outros)',
+  },
+  {
+    cod: 'ITE-005',
+    nome: 'Tópicos Especiais para Sistemas para Internet II',
+    dia: 'Sexta-feira',
+    horario: '19:00',
+    objetivo:
+      'Apresentar tecnologias ainda não estudadas ou aprofundar algum tema já visto definido pela coordenadoria do curso em cada unidade.',
+    ementa: 'Recursos e aplicações da tecnologia escolhida.',
+  },
+  {
+    cod: 'ISW-014',
+    nome: 'Projeto de Encontrabilidade',
+    dia: 'Segunda-feira',
+    horario: '19:00',
+    objetivo:
+      'Explorar estratégias que direcionam o tráfego para um site, ajudam os usuários a encontrar conteúdo dentro de um site e encorajam consultas de retorno. Discutir os Métodos de Otimização de Search Engine (SEO), estratégias de marketing, análise de tráfego e teoria em torno comportamento de busca. Investigar as relações entre usabilidade, acessibilidade, arquitetura da informação, marketing, e encontrabilidade, a fim de implementar estratégias para a criação de conteúdo encontrável.',
+    ementa:
+      'Teoria Geral. Marcação para encontrabilidade. Microformatos. Conteúdo. Rede Social. Estratégias Server-Side. Como evitar armadilhas de encontrabilidade. Pesquisas. Análises.',
+  },
+  {
+    cod: 'ISW-013',
+    nome: 'Desenvolvimento para Dispositivos Moveis II',
+    dia: 'Sábado',
+    horario: '14:00',
+    objetivo:
+      'Discutir tecnologias de desenvolvimento de aplicativos para dispositivos móveis em alguma plataforma proprietária.    ',
+    ementa:
+      'Uso de algum ambiente de desenvolvimento (framework) em plataforma proprietária: configuração do ambiente; emulação de dispositivos móveis nos computadores pessoais; interface gráfica; serviços disponíveis; banco de dados nos dispositivos. Desenvolvimento de aplicativos com uso de frameworks',
+  },
+];
